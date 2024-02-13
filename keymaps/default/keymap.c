a073d4a03ed0337236b5d0bcadea0b2214c57946
borutp <borut.pezdicek@hse.si>
2024-02-13 17:51:09 +0100

    v1.1

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_PSLS,  KC_PAST,  KC_PMNS,    KC_PPLS, 
-        KC_P7,    KC_P8,    KC_P9,      KC_DLR, 
-        KC_P4,    KC_P5,    KC_P6,      RALT(KC_E), 
-        KC_P1,    KC_P2,    KC_P3,      KC_AT,
-        KC_P0,    KC_PCMM,  KC_PDOT,    KC_PENT
+        KC_PSLS,  KC_PAST,   KC_PMNS,   KC_PPLS, 
+        KC_7,     KC_8,      KC_9,      KC_DLR, 
+        KC_4,     KC_5,      KC_6,      ALGR(KC_E), 
+        KC_1,     KC_2,      KC_3,      ALGR(KC_V),
+        KC_0,     KC_COMMA,  KC_DOT,    KC_PENT
     ),
 };
